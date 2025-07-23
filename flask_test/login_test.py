72db7ae1b217de2df6fbc911505f65c64ad2c961
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-07-23 20:31:59 +0900

    bootstrap 다운

--- flask_test/login_test.py
+++ flask_test/login_test.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -14,6 +14,11 @@ def login():
     else:
         return_data = {'auth': 'failed'}
     return jsonify(return_data)
+
+@app.route('/html_test')
+def hello_html():
+    # html file은 templates 폴더에 위치해야 함
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080')
